8637ca653f58e99b0ed58533fb0258c820020d6b
Jimah Abraham Zorwi <119421571+devjimah@users.noreply.github.com>
2024-06-05 01:07:38 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    if (document.getElementById('skillsMember').style.display == 'none') {
+        document.getElementById('skillsMember').style.display = 'block';
+    } else {
+        document.getElementById('skillsMember').style.display = 'none';
+    }
+}
